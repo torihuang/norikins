37703b2b61b535556861ab3e122cd3c0cea650c6
torihuang <victoriahuang20@gmail.com>
2016-06-08 16:51:28 -0500

    Add mongo and mongoose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   stylus = require('stylus'),
   logger = require('morgan'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV || 'development';
 
@@ -29,6 +30,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+// mongoose.connect('mongodb://localhost/multivision')
+mongoose.connect('mongodb://torihuang:dogdatanori@ds011314.mlab.com:11314/dogdata')
+var db= mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('The db is up and running...')
+})
+
+var userSchema = mongoose.Schema({username: String, password: String});
+var User = mongoose.model('User', userSchema);
+var mongoUser;
+User.findOne().exec(function(err, userDoc) {
+  mongoUser = userDoc.username;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
   res.render('partials/' + req.params.partialPath)
 })
@@ -37,6 +53,6 @@ app.get('*', function(req, res) {
   res.render('index');
 })
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Nihao from port ' + port + '...')
